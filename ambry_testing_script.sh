8725850e4b04f8940f10cb44e03ea84c329b8d6f
Jakub Ambroz <ambrojak@fel.cvut.cz>
2020-10-29 22:52:38 +0100

    Version 2.0, small changes and fixes

--- ambry_testing_script.sh
+++ ambry_testing_script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
+#Ambry's testing script for PRP Homeworks
+#Version 0.2, 24.10.2020
 
 HW=04
-REPEAT=5
+REPEAT=50
 GENERATOR=./b0b36prp-hw$HW-genref
 FOLDER_NAME=files
 TESTED_PROGRAM=./hw$HW-b0b36prp
-
-echo "Welcome to Ambry's (amborjak) script for testing PRP HW"
 
 if [ "$1" = "-osx" ]
 then
@@ -17,9 +17,9 @@ fi
 rm -rf $FOLDER_NAME
 mkdir $FOLDER_NAME
 echo "Directory was creted: $FOLDER_NAME"
-echo "------ $GENERATOR use for creating $REPEAT examples ------\n"
+echo "------ $GENERATOR use for creating $REPEAT examples ------"
 
-for i in `seq 0 $REPEAT`
+for i in $(seq 0 $REPEAT)
 do
    PROBLEM=$FOLDER_NAME/hw$HW-$i
    echo "Generate random input '$PROBLEM.in'"
@@ -41,7 +41,6 @@ else
     echo "File $TESTED_PROGRAM does not exists and cannot be used to solve the problems.\n Aborting!"
     return 1
 fi
-echo "-------- Success! -----------\n"
 
 echo "Comparing results using diff"
 for i in `seq 0 $REPEAT`
@@ -52,5 +51,32 @@ do
     echo "Comparing '$PROBLEM.err' and '$PROBLEM-my.err':"
     diff $PROBLEM.err $PROBLEM-my.err
 done
+PUBLIC_PATH=data
+echo "Using public examples"
+if [ -e $PUBLIC_PATH ];then
+    i=1
+    while :
+    do
+        if [ $i -le 9 ];then
+            PUB_FILE=data/pub0"$i"
+        else
+            PUB_FILE=data/pub"$i"
+        fi
+        if [ -f $PUB_FILE.in ];then
+            echo "Solving '$PUB_FILE.in', solution to '$PUB_FILE-my.out', errors to $PUB_FILE-my.err"
+            $TESTED_PROGRAM <$PUB_FILE.in >$PUB_FILE-my.out 2> $PUB_FILE-my.err
+            echo "Comaparing $PUB_FILE.out $PUB_FILE-my.out :"
+            diff $PUB_FILE.out $PUB_FILE-my.out
+            echo "Comaparing $PUB_FILE.err $PUB_FILE-my.err :"
+            diff $PUB_FILE.err $PUB_FILE-my.err
+        else
+            echo "Public file $PUB_FILE.in doesn't exist"
+            break
+        fi
+        i=$(($i+1))
+    done
+else
+    echo "Folder ($PUBLIC_PATH) used for public examples not found"
+fi
 echo "Testing complete. This script terminates here!"
 return 0
