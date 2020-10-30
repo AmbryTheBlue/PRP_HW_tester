807e727f7691770426ac26503864e68713019ebd
Jakub Ambroz <ambrojak@fel.cvut.cz>
2020-10-30 16:57:41 +0100

    Version 3.0, removed unnecessary echos (but they can still ve truned on using --verbose (-v) option)

--- ambry_testing_script.sh
+++ ambry_testing_script.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 #Ambry's testing script for PRP Homeworks
-#Version 2.5, 24.10.2020
+#Version 3.0, 30.10.2020
 
 HW=04
 REPEAT=10
 GENERATOR=./b0b36prp-hw$HW-genref
 FOLDER_NAME=files
 TESTED_PROGRAM=./hw$HW-b0b36prp
+VERBOSE=0
 
-echo "Welcome to Ambry's (amborjak) script for testing PRP HW, Version 2.5 - 29.10.2020"
-echo "If you want the latest version or you have found some issues/bugs go to:"
-echo "https://gitlab.fel.cvut.cz/ambrojak/prp_hw_tester"
-echo "https://github.com/AmbryTheBlue/PRP_HW_tester"
-echo "You can also request new features, but ..."
+# We use "$@" instead of $* to preserve argument-boundary information
+ARGS=$(getopt -o 'vh' --long 'verbose,help' -- "$@") || exit
+eval "set -- $ARGS"
+while true; do
+    case $1 in
+        (-v|--verbose)
+            VERBOSE=$(($VERBOSE+1)); shift;;
+        (--)  shift; break;;
+        (*)   exit 1;;           # error
+    esac
+done
+
+remaining=$("$@")
+
+if [ $VERBOSE -ge 1 ];then
+    echo "Welcome to Ambry's (amborjak) script for testing PRP HW, Version 3.0 - 30.10.2020"
+    echo "If you want the latest version or you have found some issues/bugs go to:"
+    echo "https://gitlab.fel.cvut.cz/ambrojak/prp_hw_tester"
+    echo "https://github.com/AmbryTheBlue/PRP_HW_tester"
+    echo "You can also request new features, but ..."
+fi
+
 
 if [ "$1" = "-osx" ]
 then
@@ -23,14 +41,18 @@ fi
 rm -rf $FOLDER_NAME
 mkdir $FOLDER_NAME
 echo "Directory was creted: $FOLDER_NAME"
-echo "------ $GENERATOR use for creating $REPEAT examples ------"
+echo "------ $GENERATOR will be used for creating $REPEAT examples ------"
 
 for i in $(seq 0 $REPEAT)
 do
    PROBLEM=$FOLDER_NAME/hw$HW-$i
+   if [ $VERBOSE -ge 1 ];then
    echo "Generate random input '$PROBLEM.in'"
+   fi
    $GENERATOR -generate > $PROBLEM.in 2>/dev/null #lol - all errors to NULL
+   if [ $VERBOSE -ge 1 ];then
    echo "Solve '$PROBLEM.in' and store the reference solution to '$PROBLEM.out' and reference errors to $PROBLEM.err"
+   fi
    $GENERATOR < $PROBLEM.in > $PROBLEM.out 2>$PROBLEM.err
 done
 echo "------  Generating complete! ---------\n"
@@ -40,7 +62,9 @@ if test -f $TESTED_PROGRAM;then
     for i in `seq 0 $REPEAT`
     do
         PROBLEM=$FOLDER_NAME/hw$HW-$i
+        if [ $VERBOSE -ge 1 ];then
         echo "Solving '$PROBLEM.in', solution to '$PROBLEM-my.out', errors to $PROBLEM-my.err"
+        fi
         $TESTED_PROGRAM < $PROBLEM.in > $PROBLEM-my.out 2>$PROBLEM-my.err
     done
 else
@@ -49,18 +73,46 @@ else
 fi
 
 echo "Comparing results using diff"
+FOUND_ISSUES=0
 for i in `seq 0 $REPEAT`
 do
     PROBLEM=$FOLDER_NAME/hw$HW-$i
-    echo "Comparing '$PROBLEM.out' and '$PROBLEM-my.out':"
-    diff $PROBLEM.out $PROBLEM-my.out
-    echo "Comparing '$PROBLEM.err' and '$PROBLEM-my.err':"
-    diff $PROBLEM.err $PROBLEM-my.err
+    DIFFERNCE=$(diff $PROBLEM.out $PROBLEM-my.out)
+    if [ $VERBOSE -ge 1 ];then
+        echo "Comparing '$PROBLEM.out' and '$PROBLEM-my.out':"
+        if [ "$DIFFERNCE" != "" ]; then
+            FOUND_ISSUES=$(($FOUND_ISSUES+1))
+            echo $DIFFERNCE
+        fi
+    else
+        if [ "$DIFFERNCE" != "" ]; then
+            echo "Comparing '$PROBLEM.out' and '$PROBLEM-my.out':$DIFFERNECE"
+            echo $DIFFERNCE
+            FOUND_ISSUES=$(($FOUND_ISSUES+1))
+        fi
+    fi
+
+    DIFFERNCE=$(diff $PROBLEM.err $PROBLEM-my.err)
+    if [ $VERBOSE -ge 1 ];then
+        echo "Comparing '$PROBLEM.err' and '$PROBLEM-my.err':"
+        if [ "$DIFFERNCE" != "" ]; then
+        echo $DIFFERNCE
+        FOUND_ISSUES=$(($FOUND_ISSUES+1))
+        fi
+    else
+        if [ "$DIFFERNCE" != "" ]; then
+        echo "Comparing '$PROBLEM.err' and '$PROBLEM-my.err':"
+        echo $DIFFERNCE
+        FOUND_ISSUES=$(($FOUND_ISSUES+1))
+        fi
+    fi
 done
+echo "Overall $FOUND_ISSUES issues were found!"
 PUBLIC_PATH=data
-echo "Using public examples"
+echo "-------- Using public examples --------"
 if [ -e $PUBLIC_PATH ];then
     i=1
+    FOUND_ISSUES=0
     while :
     do
         if [ $i -le 9 ];then
@@ -68,15 +120,42 @@ if [ -e $PUBLIC_PATH ];then
         else
             PUB_FILE=data/pub"$i"
         fi
+
         if [ -f $PUB_FILE.in ];then
-            echo "Solving '$PUB_FILE.in', solution to '$PUB_FILE-my.out', errors to $PUB_FILE-my.err"
             $TESTED_PROGRAM <$PUB_FILE.in >$PUB_FILE-my.out 2> $PUB_FILE-my.err
-            echo "Comaparing $PUB_FILE.out $PUB_FILE-my.out :"
-            diff $PUB_FILE.out $PUB_FILE-my.out
-            echo "Comaparing $PUB_FILE.err $PUB_FILE-my.err :"
-            diff $PUB_FILE.err $PUB_FILE-my.err
+            if [ $VERBOSE -ge 1 ];then
+                echo "Solving '$PUB_FILE.in', solution to '$PUB_FILE-my.out', errors to $PUB_FILE-my.err"
+                DIFFERNCE=$(diff $PUB_FILE.out $PUB_FILE-my.out)
+                echo "Comaparing $PUB_FILE.out $PUB_FILE-my.out :"
+                if [ "$DIFFERNCE" != "" ]; then
+                    echo $DIFFERNCE
+                    FOUND_ISSUES=$(($FOUND_ISSUES+1))
+                fi
+                DIFFERNCE=$(diff $PUB_FILE.err $PUB_FILE-my.err)
+                echo "Comaparing $PUB_FILE.err $PUB_FILE-my.err :"
+                if [ "$DIFFERNCE" != "" ]; then
+                    echo $DIFFERNCE
+                    FOUND_ISSUES=$(($FOUND_ISSUES+1))
+                fi
+            else
+                DIFFERNCE=$(diff $PUB_FILE.out $PUB_FILE-my.out)
+                if [ "$DIFFERNCE" != "" ]; then
+                    echo "Comaparing $PUB_FILE.out $PUB_FILE-my.out :"
+                    echo $DIFFERNCE
+                    FOUND_ISSUES=$(($FOUND_ISSUES+1))
+                fi
+                DIFFERNCE=$(diff $PUB_FILE.err $PUB_FILE-my.err)
+                if [ "$DIFFERNCE" != "" ]; then
+                    echo "Comaparing $PUB_FILE.err $PUB_FILE-my.err :"
+                    echo $DIFFERNCE
+                    FOUND_ISSUES=$(($FOUND_ISSUES+1))
+                fi
+            fi
         else
-            echo "Public file $PUB_FILE.in doesn't exist"
+            if [ $VERBOSE -ge 1 ];then
+                echo "Public file $PUB_FILE.in doesn't exist. Testing pub files ends here."
+            fi
+            echo "Overall $FOUND_ISSUES issues were found!"
             break
         fi
         i=$(($i+1))
